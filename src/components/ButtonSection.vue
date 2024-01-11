d6a91d0692b82b3e140132d76b7861b921e83220
Chopper <matheus.ricieri@hotmail.com>
2024-01-11 17:27:59 -0300

    Update ButtonSection.vue
    Add CSS classes

--- src/components/ButtonSection.vue
+++ src/components/ButtonSection.vue
@@ -1,8 +1,26 @@
 <template>
-    <button>Saving Throw</button>
-    <button>+1 Failure</button>
-    <button>+1 Succsess</button>
-    <button>Clear</button>
-    <button>Clear all</button>
-    <button>Remove</button>
+    <div
+        class="grid grid-cols-2 gap-x-2 gap-y-4 mt-4 px-8 py-4 rounded-md bg-transparent-black text-zinc-300 md:mt-0"
+    >
+        <button
+            class="bg-blue-900 text-white hover:bg-blue-800 py-2 px-4 rounded-md col-start-1 col-span-2"
+        >
+            Saving Throw
+        </button>
+        <button
+            class="bg-gray-700 text-white hover:bg-gray-600 py-2 px-4 rounded-md col-start-1"
+        >
+            Clear
+        </button>
+        <button
+            class="bg-gray-700 text-white hover:bg-gray-600 py-2 px-4 rounded-md col-start-2"
+        >
+            Clear all
+        </button>
+        <button
+            class="bg-red-900 text-white hover:bg-red-800 py-2 px-4 rounded-md col-start-1 col-span-2"
+        >
+            Remove Player
+        </button>
+    </div>
 </template>
