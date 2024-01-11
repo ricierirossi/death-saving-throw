8220ef9b53536962c830b87144379c9d585794ed
Chopper <matheus.ricieri@hotmail.com>
2024-01-11 12:05:57 -0300

    Create ButtonSection component

---
+++ src/components/ButtonSection.vue
@@ -0,0 +1,8 @@
+<template>
+    <button>Saving Throw</button>
+    <button>+1 Failure</button>
+    <button>+1 Succsess</button>
+    <button>Clear</button>
+    <button>Clear all</button>
+    <button>Remove</button>
+</template>
