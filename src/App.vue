cb9ca0cdb7858ba09bb9c3a65260effd71894bc9
Chopper <matheus.ricieri@hotmail.com>
2024-01-11 17:28:41 -0300

    Update App.vue
    Add CSS classes

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,27 @@ import ButtonSection from "./components/ButtonSection.vue";
 <template>
     <div class="h-screen bg-lava bg-cover bg-bottom p-5">
         <header>
-            <h1 class="text-4xl">Saving Death Throw</h1>
+            <h1 class="text-4xl text-gray-500 text-">Saving Death Throw</h1>
         </header>
-        <main class="mt-8 px-8 py-4 bg-transparent-black text-zinc-300">
-            <div class="flex justify-between">
-                <section>
-                    <StatusTable />
-                    <span class="material-symbols-outlined">add_circle</span>
-                </section>
-                <section>
-                    <ButtonSection />
-                </section>
+        <main class="mt-6">
+            <div
+                class="w-full flex flex-col md:flex-row md:justify-center md:gap-10"
+            >
+                <StatusTable />
+                <ButtonSection />
             </div>
         </main>
-        <footer class="absolute bottom-0 text-white">
+        <footer
+            class="absolute bottom-0 left-0 w-screen text-center text-white"
+        >
+            <a href="https://github.com/ricierirossi" target="_blank"
+                >Designed with ♥ by ricierirossi</a
+            >
+            <span> • </span>
             <a
                 href="https://br.freepik.com/vetores-gratis/vulcao-quente-da-paisagem-com-fluxo-de-lava_25377031.htm#query=background%20site%20rpg&position=0&from_view=search&track=ais&uuid=219049bc-68f4-4fbf-bd76-9dacdce33838"
                 target="_blank"
-                >Image by gstudioimagen1</a
+                >Background image by gstudioimagen1</a
             >
             on Freepik
         </footer>
