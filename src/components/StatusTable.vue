391448fb61981325aa1e989e1162dbc8e37095f0
Chopper <matheus.ricieri@hotmail.com>
2024-01-15 12:21:47 -0300

    Add functions, update info
    Add functions to insert characters, sum failures, sum successes;
    Add failures and successes buttons and icons

--- src/components/StatusTable.vue
+++ src/components/StatusTable.vue
@@ -1,49 +1,89 @@
 <template>
     <div class="px-8 py-4 rounded-md bg-transparent-black text-zinc-300">
-        <table>
-            <thead>
-                <tr class="gap-5">
-                    <th></th>
-                    <th>Player</th>
-                    <th>Failures</th>
-                    <th>Successes</th>
-                    <th>Status</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="p in 5">
-                    <input type="checkbox" />
-                    <td>Character {{ p }}</td>
-                    <td>
+        <div class="grid">
+            <div class="grid grid-cols-7 text-center">
+                <span></span>
+                <span>Character</span>
+                <span>Failures</span>
+                <span>Successes</span>
+                <span>Status</span>
+            </div>
+            <div
+                v-for="(character, key) in statusStore.characters"
+                :key="character.id"
+            >
+                <div class="grid grid-cols-7 text-center">
+                    <span>{{ character.name }}</span>
+                    <span>
                         <span
-                            v-for="s in 3"
-                            class="material-symbols-outlined hover:cursor-pointer"
+                            v-for="failures in character.failures"
+                            class="material-symbols-outlined hover:cursor-pointer text-red-800"
+                            >skull</span
                         >
-                            skull
-                        </span>
-                    </td>
-                    <td>
+                    </span>
+                    <span>
                         <span
-                            v-for="h in 3"
-                            class="material-symbols-outlined hover:cursor-pointer"
+                            v-for="failures in character.successess"
+                            class="material-symbols-outlined hover:cursor-pointer text-green-800"
+                            >health_and_safety</span
                         >
-                            health_and_safety
-                        </span>
-                    </td>
-                    <td>Dead</td>
-                </tr>
-            </tbody>
-        </table>
+                    </span>
+                    <span>{{ character.status }}</span>
+                    <button
+                        class="bg-red-900 rounded-md hover:bg-red-800"
+                        @click="addFailure(key)"
+                    >
+                        +1 Failure
+                    </button>
+                    <button
+                        class="bg-green-900 rounded-md hover:bg-green-800"
+                        @click="addSuccess(key)"
+                    >
+                        +1 Success
+                    </button>
+                </div>
+            </div>
+        </div>
         <div class="flex mt-6 gap-x-3">
             <input
                 type="text"
                 placeholder="Insert character name"
-                class="text-black bg-transparent border-b-2"
+                class="text-zinc-300 bg-transparent border-b-2"
+                v-model="charName"
             />
-            <span class="material-symbols-outlined hover:cursor-pointer"
+            <span
+                class="material-symbols-outlined hover:cursor-pointer"
+                @click="insertChar"
                 >add_circle</span
             >
         </div>
     </div>
 </template>
+<script setup>
+import { useStatusStore } from "../../stores/StatusStore";
+import { ref } from "vue";
+
+const statusStore = useStatusStore();
+const charName = ref("");
+
+const insertChar = () => {
+    if (charName.value) {
+        statusStore.addCharacter({
+            id: new Date(Date.now()).getTime(),
+            name: charName.value,
+            failures: 0,
+            successess: 0,
+            status: "",
+        });
+    }
+    charName.value = "";
+};
+
+const addFailure = (characterKey) => {
+    statusStore.addCharacterFail(characterKey);
+};
+
+const addSuccess = (characterKey) => {
+    statusStore.addCharacterSuccess(characterKey);
+};
+</script>
