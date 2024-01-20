f64a1dfe06c6bd62d85629cb306b0316f6affe34
Chopper <matheus.ricieri@hotmail.com>
2024-01-20 15:12:37 -0300

    Remove footer

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import ButtonSection from "./components/ButtonSection.vue";
 </script>
 
 <template>
-    <div class="h-screen bg-lava bg-cover bg-bottom py-5 px-2">
+    <div class="min-h-screen bg-lava bg-cover bg-bottom py-5 px-2">
         <header>
             <h1 class="text-4xl text-gray-500 text-">Saving Death Throw</h1>
         </header>
@@ -16,19 +16,5 @@ import ButtonSection from "./components/ButtonSection.vue";
                 <ButtonSection />
             </div>
         </main>
-        <!-- <footer
-            class="absolute bottom-0 left-0 w-screen text-center text-white"
-        >
-            <a href="https://github.com/ricierirossi" target="_blank"
-                >Designed with ♥ by ricierirossi</a
-            >
-            <span> • </span>
-            <a
-                href="https://br.freepik.com/vetores-gratis/vulcao-quente-da-paisagem-com-fluxo-de-lava_25377031.htm#query=background%20site%20rpg&position=0&from_view=search&track=ais&uuid=219049bc-68f4-4fbf-bd76-9dacdce33838"
-                target="_blank"
-                >Background image by gstudioimagen1</a
-            >
-            on Freepik
-        </footer> -->
     </div>
 </template>
