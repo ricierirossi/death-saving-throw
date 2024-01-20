5e5862ad912867cf56f013217de5d8429f35af44
Chopper <matheus.ricieri@hotmail.com>
2024-01-20 15:13:45 -0300

    Update ButtonSection.vue
    Add input from StatusTable

--- src/components/ButtonSection.vue
+++ src/components/ButtonSection.vue
@@ -2,16 +2,40 @@
     <div
         class="grid grid-cols-2 gap-x-2 gap-y-4 mt-4 px-8 py-4 rounded-md bg-transparent-black text-zinc-300 md:mt-0"
     >
-        <div class="grid col-start-1 col-span-2">
+        <div
+            class="flex gap-x-3 bg-slate-800 py-2 px-4 rounded-md col-start-1 col-span-2"
+        >
+            <input
+                type="text"
+                placeholder="Insert character name"
+                class="text-zinc-300 bg-transparent border-b-2 flex-grow"
+                v-model="characterName"
+                @keyup.enter="insertCharacter"
+            />
+            <span
+                class="material-symbols-outlined hover:cursor-pointer hover:text-gray-400"
+                @click="insertCharacter"
+                >add_circle</span
+            >
+        </div>
+        <div class="flex flex-col col-start-1 col-span-2">
             <button
                 class="bg-blue-900 hover:bg-blue-800 py-2 px-4 rounded-md"
                 @click="rollDice"
             >
                 Saving Throw
             </button>
-            <span class="text-center">
-                {{ diceThrow }}
-            </span>
+            <div class="flex justify-center min-h-7">
+                <span
+                    class="text-center text-lg font-bold bg-blue-950 rounded-b-md min-w-64"
+                    :class="{
+                        'text-red-600': diceThrow === 1,
+                        'text-green-600': diceThrow === 20,
+                    }"
+                >
+                    {{ diceThrow }}
+                </span>
+            </div>
         </div>
 
         <button
@@ -52,7 +76,22 @@ import { useStatusStore } from "../../stores/StatusStore";
 import { ref } from "vue";
 
 const statusStore = useStatusStore();
+const characterName = ref("");
 const diceThrow = ref("");
+
+const insertCharacter = () => {
+    if (characterName.value) {
+        statusStore.addCharacter({
+            id: new Date(Date.now()).getTime(),
+            name: characterName.value,
+            failures: 0,
+            successess: 0,
+            status: "",
+            selected: false,
+        });
+    }
+    characterName.value = "";
+};
 
 const clearSomeSavings = () => {
     statusStore.clearSomeSavings();
@@ -60,6 +99,7 @@ const clearSomeSavings = () => {
 
 const clearAll = () => {
     statusStore.clearAllSavings();
+    diceThrow.value = "";
 };
 
 const removeCharacters = () => {
@@ -71,7 +111,7 @@ const rollDice = () => {
 };
 
 const addFailure = () => {
-    statusStore.addCharacterFail();
+    statusStore.addCharacterFailure();
 };
 
 const addSuccess = () => {
