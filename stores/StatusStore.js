e6e387429e5db6adb27704f108e77101027b738e
Chopper <matheus.ricieri@hotmail.com>
2024-01-22 10:20:37 -0300

    Removed populated data

--- stores/StatusStore.js
+++ stores/StatusStore.js
@@ -2,41 +2,7 @@ import { defineStore } from "pinia";
 
 export const useStatusStore = defineStore("savings", {
     state: () => ({
-        // ! Remove mocked data
-        characters: [
-            {
-                id: 0,
-                name: "Yix",
-                failures: 2,
-                successess: 2,
-                status: "Risky",
-                selected: false,
-            },
-            {
-                id: 1,
-                name: "Varis",
-                failures: 3,
-                successess: 2,
-                status: "Dead",
-                selected: false,
-            },
-            {
-                id: 2,
-                name: "Kriv",
-                failures: 0,
-                successess: 3,
-                status: "Alive",
-                selected: false,
-            },
-            {
-                id: 3,
-                name: "Galaeril",
-                failures: 0,
-                successess: 0,
-                status: "",
-                selected: false,
-            },
-        ],
+        characters: [],
     }),
     actions: {
         addCharacter(character) {
@@ -107,23 +73,4 @@ export const useStatusStore = defineStore("savings", {
             });
         },
     },
-    // getters: {
-    //     statusChange: (state) => {
-    //         state.characters.forEach((character) => {
-    //             if (character.failures === 0 && character.successess === 0) {
-    //                 return (character.status = "");
-    //             }
-    //             if (character.failures === 3 && character.successess === 3) {
-    //                 return (character.status = "Schrodinger's cat");
-    //             }
-    //             if (character.failures === 3) {
-    //                 return (character.status = "Dead");
-    //             }
-    //             if (character.successess === 3) {
-    //                 return (character.status = "Alive");
-    //             }
-    //             return (character.status = "Risky");
-    //         });
-    //     },
-    // },
 });
