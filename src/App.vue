80dc631b74d8a4ffdab75dbd79456ad45f0b3533
Chopper <matheus.ricieri@hotmail.com>
2024-02-14 13:17:57 -0300

    Add responsiveness
    Event listener to screen width

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <script setup>
 import StatusTable from "./components/StatusTable.vue";
-import ButtonSection from "./components/ButtonSection.vue";
 import ButtonsBar from "./components/ButtonsBar.vue";
+import ButtonsBox from "./components/ButtonsBox.vue";
+
+import { ref, onMounted, onUnmounted } from "vue";
+
+const windowSize = ref(window.innerWidth);
+const breakPoint = ref(1024);
+
+onMounted(() => {
+    window.addEventListener("resize", () => {
+        windowSize.value = window.innerWidth;
+    });
+});
+onUnmounted(() => {
+    window.removeEventListener("resize", () => {
+        windowSize.value = window.innerWidth;
+    });
+});
 </script>
 
 <template>
     <div
-        class="min-h-screen gradient bg-cover bg-bottom text-zinc-300 select-none"
+        class="min-h-screen gradient bg-cover bg-bottom text-zinc-300 select-none xl:flex xl:flex-col xl:justify-center"
     >
         <header>
             <h1 class="py-2 md:pt-8 text-center text-4xl">
@@ -14,10 +30,10 @@ import ButtonsBar from "./components/ButtonsBar.vue";
             </h1>
         </header>
         <main class="mt-1 md:mt-5 pb-5">
-            <div class="md:flex md:justify-center md:mx-3 md:gap-x-2">
+            <div class="md:mx-3 lg:flex lg:justify-around lg:gap-2">
                 <StatusTable />
-                <!-- <ButtonSection /> -->
-                <ButtonsBar />
+                <ButtonsBar v-if="windowSize < breakPoint" />
+                <ButtonsBox v-if="windowSize >= breakPoint" />
             </div>
         </main>
     </div>
