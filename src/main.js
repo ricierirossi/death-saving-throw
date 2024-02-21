49b30e1828dded3d262c1c24013afde041f3d9ed
Chopper <matheus.ricieri@hotmail.com>
2024-02-21 16:39:31 -0300

    Reorder code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import "./style.css";
-
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import App from "./App.vue";
+import { createPinia } from "pinia";
+import "./style.css";
 
 const pinia = createPinia();
 
